50163a5627ddd7c30d2f03de576a048b2020cd23
202101912 <202101912@student.polytechnic.bh>
2026-05-24 02:54:34 +0300

    added animation, changed the coin cellect, added basic screen ui for scoring

--- Assets/_Core/Scripts/SceneController.cs
+++ Assets/_Core/Scripts/SceneController.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 using System.Text.RegularExpressions;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneController : MonoBehaviour
 {
+    private GameObject[] coins; // Array to hold all coin objects in the scene
     public  GameObject spawnPoint; // Reference to the spawn point in the scene
+    public TextMeshProUGUI Score; // Reference to the TextMeshPro for displaying coin count
     private void Start()
     {
+         //Get total coin object placed in the current level
+        coins = GameObject.FindGameObjectsWithTag("Coin");
+        Debug.Log("Total coins in the scene: " + coins.Length);
         // Spawn player at the SpawnPoint in the current scene
         SpawnPlayerAtSpawnPoint();
+        //Initialize the coin count display for the current level
+        LevelLayout();
     }
 
     public void SpawnPlayerAtSpawnPoint()
@@ -30,6 +38,27 @@ public class SceneController : MonoBehaviour
         {
             Debug.LogError("No Player found in the scene!");
         }
+    }
+
+    public void LevelLayout()
+    {
+
+        //Get total coins in player inventory for current level
+        PlayerInventory playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+        Debug.Log("Player's current coin count: " + playerInventory.coinCount);
+
+        //Formatic way to display the coin count in the UI can be implemented here, 
+        //"cointCount / coins.Length"
+        if (Score != null)
+        {
+            Score.text =  playerInventory.coinCount + " / " + coins.Length;
+        }
+        else
+        {
+            Debug.LogError("Score TextMesh reference is not set!");
+        }
+
+
     }
 
     public void LoadNextScene( string sceneName)
